39a4c0f035161e9a6cff124269569f2205dfe848
Konstantin <keynicorn@gmail.com>
2023-03-20 20:25:41 +0200

    Create renderState.js

---
+++ Front End Development Libraries/React/renderState.js
@@ -0,0 +1,19 @@
+{ /* Render State in the User Interface */ }
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}
+        </h1>
+      </div>
+    );
+  }
+};
