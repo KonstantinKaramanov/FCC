d6200f502f67e16040844c3e999704503121585b
Konstantin <keynicorn@gmail.com>
2023-03-21 11:17:35 +0200

    Create renderInUI.js

---
+++ Front End Development Libraries/React/renderInUI.js
@@ -0,0 +1,19 @@
+{ /* State in the User Interface Another Way */ }
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    const name = this.state.name
+    return (
+      <div>
+        <h1>{name}
+        </h1>
+      </div>
+    );
+  }
+};
